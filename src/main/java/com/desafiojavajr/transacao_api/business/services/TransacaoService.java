6b30391e20832d3dee4546a7e6d65e102772c7c0
Gabriel Azevedo <g.azevedo0121@gmail.com>
2025-09-22 16:40:16 -0300

    feat: falta finalizar logica

--- src/main/java/com/desafiojavajr/transacao_api/business/services/TransacaoService.java
+++ src/main/java/com/desafiojavajr/transacao_api/business/services/TransacaoService.java
@@ -1,48 +1,55 @@
 package com.desafiojavajr.transacao_api.business.services;
 
-import java.time.OffsetDateTime;
-import java.util.ArrayList;
 
-import org.springframework.stereotype.Service;
-
-import com.desafiojavajr.transacao_api.infrastructure.exceptions.UnprocessableEntity;
-
+import com.javanauta.transacao_api.controller.dtos.TransacaoRequestDTO;
+import com.javanauta.transacao_api.infrastructure.exceptions.UnprocessableEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class TransacaoService {
 
-    //armazenamento em memoria definido
     private final List<TransacaoRequestDTO> listaTransacoes = new ArrayList<>();
 
-    public void adicionaTransacoes(TransacoesRequestDTO dto){
-        log.info( "Inicar o processo de gravar transacoes");
+    public void adicionarTransacoes(TransacaoRequestDTO dto){
+
+        log.info("Iniciado o processamento de gravar transações " + dto);
 
         if(dto.dataHora().isAfter(OffsetDateTime.now())){
             log.error("Data e hora maiores que a data atual");
-            throw new UnprocessableEntity("Data e hora maiores que a data atual");
+            throw new UnprocessableEntity("Data e hora maiores que a data e hora atuais");
         }
-        if(dto.valor()<0){
-            throw new UnprocessableEntity("O valor nao pode ser menor que 0");
+        if(dto.valor() < 0){
+            log.error("Valor não pode ser menor que 0");
+            throw new UnprocessableEntity("Valor não pode ser menor que 0");
+        }
 
-        }
         listaTransacoes.add(dto);
+        log.info("Transacoes adicionadas com sucesso");
     }
 
     public void limparTransacoes(){
+        log.info("Iniciado processamento para deletar transações");
         listaTransacoes.clear();
+        log.info("Transações deletadas com sucesso");
     }
 
     public List<TransacaoRequestDTO> buscarTransacoes(Integer intervaloBusca){
-        // data e hora atual menos os segundos do intervalo de busca
+        log.info("Inicadas buscas de transações por tempo " + intervaloBusca);
         OffsetDateTime dataHoraIntervalo = OffsetDateTime.now().minusSeconds(intervaloBusca);
 
+        log.info("Retorno de transações com sucesso");
         return listaTransacoes.stream()
-        .filter(transacoes -> transacoes.dataHora()
-            .isAfter(dataHoraIntervalo)).toLust();
-//falta o controller
+                .filter(transacao -> transacao.dataHora()
+                        .isAfter(dataHoraIntervalo)).toList();
+
     }
+
 }
