7a0613367b5b44e668175759e1de71e74d58a264
Gabriel Azevedo <g.azevedo0121@gmail.com>
2025-09-22 17:09:32 -0300

    docs: comentaa estatisticacontroler

--- src/main/java/com/desafiojavajr/transacao_api/controller/EstatisticasController.java
+++ src/main/java/com/desafiojavajr/transacao_api/controller/EstatisticasController.java
@@ -1,11 +1,11 @@
 package com.desafiojavajr.transacao_api.controller;
-
-import com.javanauta.transacao_api.business.services.EstatisticasService;
-import com.javanauta.transacao_api.controller.dtos.EstatisticasResponseDTO;
+import com.desafiojavajr.transacao_api.business.services.EstatisticasService;
+import com.desafiojavajr.transacao_api.controller.dtos.EstatisticasResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,23 +13,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/estatistica")
+@RequestMapping("/estatistica") // define a url base
 @RequiredArgsConstructor
 public class EstatisticasController {
 
+    // injeta o servico de estatisticas
     private final EstatisticasService estatisticasService;
 
-    @GetMapping
-    @Operation(description = "Endpoint responsável por buscar estatísticas de transações")
+    @GetMapping //get
+    @Operation(description = "endpoint responsavel por buscar estatisticas de transacoes")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Busca efetuada com sucesso"),
-            @ApiResponse(responseCode = "400", description = "Erro na busca de estatísticas de transações"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+            @ApiResponse(responseCode = "200", description = "busca efetuada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "erro na busca de estatisticas de transacoes"),
+            @ApiResponse(responseCode = "500", description = "erro interno do servidor")
     })
     public ResponseEntity<EstatisticasResponseDTO> buscarEstatisticas(
-            @RequestParam(value = "intervaloBusca", required = false, defaultValue = "60") Integer intervaloBusca){
+            // define um parametro opcional para o intervalo de tempo com valor padrao de 60 segundos
+            @RequestParam(value = "intervaloBusca", required = false, defaultValue = "60") Integer intervaloBusca) {
+        // chama o servico para calcular e retorna o resultado com status http 200 ok
         return ResponseEntity.ok(
                 estatisticasService.calcularEstatisticasTransacoes(intervaloBusca));
     }
-
 }
