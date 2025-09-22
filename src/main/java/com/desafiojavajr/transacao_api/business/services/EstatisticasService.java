f1394c5c72b215cf01c4755960d5577979a6e4ae
Gabriel Azevedo <g.azevedo0121@gmail.com>
2025-09-22 17:04:34 -0300

    docs: comenta estatisticaservice

--- src/main/java/com/desafiojavajr/transacao_api/business/services/EstatisticasService.java
+++ src/main/java/com/desafiojavajr/transacao_api/business/services/EstatisticasService.java
@@ -1,11 +1,10 @@
 package com.desafiojavajr.transacao_api.business.services;
 
-import com.javanauta.transacao_api.controller.dtos.EstatisticasResponseDTO;
-import com.javanauta.transacao_api.controller.dtos.TransacaoRequestDTO;
+import com.desafiojavajr.transacao_api.controller.dtos.EstatisticasResponseDTO;
+import com.desafiojavajr.transacao_api.controller.dtos.TransacaoRequestDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
 
@@ -14,34 +13,31 @@ import java.util.List;
 @Slf4j
 public class EstatisticasService {
 
+    // injeta o servico de transacao para poder usar seus metodos
     public final TransacaoService transacaoService;
 
-
+    // metodo principal que calcula as estatisticas
     public EstatisticasResponseDTO calcularEstatisticasTransacoes(Integer intervaloBusca) {
 
-        log.info("Iniciada busca de estatísticas de transações pelo período de tempo " + intervaloBusca);
-
-        long start = System.currentTimeMillis();
-
+        log.info("iniciada busca de estatisticas de transacoes pelo periodo de tempo " + intervaloBusca);
+        // busca as transacoes recentes usando o transacaoservice
         List<TransacaoRequestDTO> transacoes = transacaoService.buscarTransacoes(intervaloBusca);
-
+        // se a lista de transacoes estiver vazia retorna um dto com valores zerados
         if (transacoes.isEmpty()) {
             return new EstatisticasResponseDTO(0L, 0.0, 0.0, 0.0, 0.0);
         }
 
+        // usa a classe DoubleSummaryStatistics para calcular tudo de uma vez de forma eficiente
         DoubleSummaryStatistics estatisticasTransacoes = transacoes.stream()
                 .mapToDouble(TransacaoRequestDTO::valor).summaryStatistics();
-
-        long finish = System.currentTimeMillis();
-        long tempoRequisicao = finish - start;
-        System.out.println("Tempo de requisição: " + tempoRequisicao + " milissegundos");
-
-        log.info("Estatisticas retornadas com sucesso");
-        return new EstatisticasResponseDTO(estatisticasTransacoes.getCount(),
+        log.info("estatisticas retornadas com sucesso");
+        // cria e retorna o dto com os resultados do calculo
+        return new EstatisticasResponseDTO(
+                estatisticasTransacoes.getCount(),
                 estatisticasTransacoes.getSum(),
                 estatisticasTransacoes.getAverage(),
                 estatisticasTransacoes.getMin(),
-                estatisticasTransacoes.getMax());
+                estatisticasTransacoes.getMax()
+        );
     }
-
 }
