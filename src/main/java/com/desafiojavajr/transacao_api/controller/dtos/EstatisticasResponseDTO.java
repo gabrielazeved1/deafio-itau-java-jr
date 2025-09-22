53eced571b75510f2dd42af46672f05512a07327
Gabriel Azevedo <g.azevedo0121@gmail.com>
2025-09-22 17:14:11 -0300

    feat:record de saida

--- src/main/java/com/desafiojavajr/transacao_api/controller/dtos/EstatisticasResponseDTO.java
+++ src/main/java/com/desafiojavajr/transacao_api/controller/dtos/EstatisticasResponseDTO.java
@@ -1,9 +1,8 @@
 package com.desafiojavajr.transacao_api.controller.dtos;
-
+// este é um record que representa a saida das estatisticas
 public record EstatisticasResponseDTO(Long count,
                                       Double sum,
                                       Double avg,
                                       Double min,
                                       Double max) {
-
 }
