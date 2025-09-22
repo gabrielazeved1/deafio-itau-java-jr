fda0d0a46394275b224c5e9a37e05b175f69a937
Gabriel Azevedo <g.azevedo0121@gmail.com>
2025-09-22 17:15:02 -0300

    feat:adiciona tratamento de erros customizados

--- src/main/java/com/desafiojavajr/transacao_api/infrastructure/exceptions/UnprocessableEntity.java
+++ src/main/java/com/desafiojavajr/transacao_api/infrastructure/exceptions/UnprocessableEntity.java
@@ -1,8 +1,8 @@
 package com.desafiojavajr.transacao_api.infrastructure.exceptions;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+// esta é uma excecao customizada para erros de negocio que resultam em http 422
 @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
 public class UnprocessableEntity extends RuntimeException {
     public UnprocessableEntity(String message) {
